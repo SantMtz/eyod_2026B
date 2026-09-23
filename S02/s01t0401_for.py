d74cb7d80c4b2e298fbf8e19db82e66dec622efb
SantMtz <ianmartinez445566@gmail.com>
2026-09-23 03:33:11 +0000

    ⭐ s01t0401 for Finaliza toma de tiempo de la suma de 100 numeros usando for

--- S02/s01t0401_for.py
+++ S02/s01t0401_for.py
@@ -1,31 +1,32 @@
 """
-Escribir un programa que calcule la suma de los "n" numeros naturales. Por ejemplo si n= 100, el programa calculara la suya del 1 al 100
+escrbir un programa que calcule 
+la suma de los "n" numeros naturales.
+por elemplo si n= 100, el progama 
+calculara la suma de 1 al 100
+42
 
 """
-# Importamos  biblioteca 
+# importamos bliblioteca time
 import time
-#Tomando el tiempo incial
-# Creando una marca de tiempo 
-timestamp_01 = time.time()
 
-# Programa que calcula las suma # de los "n" numeros naturales 
-n = 100
+# Crear una marca de tiempo
+timestamp_01 = time.time()
+# progama que calcule las sumas 
+# de los "n" numeros naturales 
+n= 3500
 sum = 0
 
-# Ciclo for
-for number in range (1,n+1):
-   sum = sum + number
-   # 1: sum <-0 + 1
-   # sum = 1
-   # 2: sum <- 1 + 2
-   # sum = 3 
-   #3: sum <- 3 + 3
-   # ...
-   # 100: sum <- anterior a Sum(o se pone sum_(-1)) + 100
-print(f"La suma de 1 hasta {n} es: {sum}")
+#ciclo for
+for number in range(1,n+1):
+    sum = sum + number 
+    # l: sum <- 0 + 1
+    # sum = 1
+    # 2: sum <- 1 + 2 
+    # sum = 3 
+    # 3: sum <- 3 +3 
+    # ...
+    # 100: SUM : <- sum_(-1) + 75
+print(f"la suma de 1 hasta {n} es: {sum}")
 
-# Tomando el tiempo final 
 timestamp_02 = time.time()
-
-# Impresion del tiempo de ejecución 
-print(f"Tiempo de ejecución: {(timestamp_02 - timestamp_01) * 1e6} μs")
+print(f"Tiempo de ejecución: {(timestamp_02 - timestamp_01) * 1e6:.2f} μs")
