e9a9bd96371e38e0759898890154dfc7c78409aa
SantMtz <ianmartinez445566@gmail.com>
2026-09-24 16:52:23 +0000

    S01T04 Prueba de Rendimiento

--- S02/s01t0401_for.py
+++ S02/s01t0401_for.py
@@ -1,32 +1,39 @@
-"""
-escrbir un programa que calcule 
-la suma de los "n" numeros naturales.
-por elemplo si n= 100, el progama 
-calculara la suma de 1 al 100
-42
-
-"""
-# importamos bliblioteca time
 import time
 
-# Crear una marca de tiempo
-timestamp_01 = time.time()
-# progama que calcule las sumas 
-# de los "n" numeros naturales 
-n= 3500
-sum = 0
+# Funcion que suma los
+# primeros "n" numreso naturles
+def sum_of_n(n):
+    total_sum = 0
+    # Sumando los "n" numeros
+    # Ciclo for
+    for number in range(1,n+1):
+        total_sum = total_sum + number
+    # Retornando el total de la suma
+    return total_sum
 
-#ciclo for
-for number in range(1,n+1):
-    sum = sum + number 
-    # l: sum <- 0 + 1
-    # sum = 1
-    # 2: sum <- 1 + 2 
-    # sum = 3 
-    # 3: sum <- 3 +3 
-    # ...
-    # 100: SUM : <- sum_(-1) + 75
-print(f"la suma de 1 hasta {n} es: {sum}")
+# Variable para guardar
+# El data set
+dataset = []
 
-timestamp_02 = time.time()
-print(f"Tiempo de ejecución: {(timestamp_02 - timestamp_01) * 1e6:.2f} μs")
+for repetition in range(1,11):
+    # ⏱️ Tomo el tiempo 1 (inicial)
+    timestamp_01 = time.time()
+
+    # Sumo los "n" números
+    n = repetition*500
+    # Guardo el resultado en result
+    result = sum_of_n(n)
+
+    # ⏱️ Tomando el tiempo final
+    timestamp_02 = time.time()
+
+    # Calculando el tiempo
+    elapsed_time = round((timestamp_02-timestamp_01) * 1e6,2)
+
+    # Agregar la tripleta de los
+    # datos al dataset
+    dataset.append( (n,elapsed_time,result) )
+
+# Imprimir el dataset
+for tup in dataset:
+    print(tup)
